b6adb7b534d2e0cc7bca3f8980221d04bb3ed76e
developershere <129280539+developershere@users.noreply.github.com>
2023-06-20 11:33:45 +0530

    Update api.js

--- src/webapi/api.js
+++ src/webapi/api.js
@@ -1,16 +1,18 @@
+const baseurl="https://pustakalaya-beckbone.onrender.com"
+
 export const apiEndPoint = {
-    CATEGORY_API : "/category/list",
-    BOOK_LIST:"/book/list",
-    FREE_BOOK_API:"/book/freebooklist",
-    USER_LIST:"/user/userList",
-    ADMIN_SIGNIN:"/admin/signIn",
-    REMOVE_CATEGORY:"/category/remove",
-    ADD_CATEGORY:"/category/add",
-    EDIT_CATEGORY:"/category/edit",
-    REMOVE_BOOK:"/book/update-book",
-    TOTAL_USERS:"/user/userList",
-    TOTAL_BOOKS:"/book/totalbook",
-    TOTAL_FREE_BOOKS:"/book/freeBookList",
-    TOTAL_PENDING_BOOKS:"/book/totalpendingbook"
+    CATEGORY_API :baseurl+"/category/list",
+    BOOK_LIST:baseurl+"/book/list",
+    FREE_BOOK_API:baseurl+"/book/freebooklist",
+    USER_LIST:baseurl+"/user/userList",
+    ADMIN_SIGNIN:baseurl+"/admin/signIn",
+    REMOVE_CATEGORY:baseurl+"/category/remove",
+    ADD_CATEGORY:baseurl+"/category/add",
+    EDIT_CATEGORY:baseurl+"/category/edit",
+    REMOVE_BOOK:baseurl+"/book/update-book",
+    TOTAL_USERS:baseurl+"/user/userList",
+    TOTAL_BOOKS:baseurl+"/book/totalbook",
+    TOTAL_FREE_BOOKS:baseurl+"/book/freeBookList",
+    TOTAL_PENDING_BOOKS:baseurl+"/book/totalpendingbook"
 
 }
